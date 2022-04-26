377c5726861ed29d787b72614d51c34c98da7876
Gopal1999 <goyalgopal359@gmail.com>
2022-04-26 21:34:36 +0530

    Admin Api added

--- src/main/java/com/example/spemajorbackend/controller/AdminController.java
+++ src/main/java/com/example/spemajorbackend/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/storagepoint")
+@RequestMapping("/storagepoint/admin")
 public class AdminController
 {
     @Autowired
