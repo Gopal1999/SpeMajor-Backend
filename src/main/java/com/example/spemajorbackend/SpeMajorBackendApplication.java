43267843a7db51ccc687552aa745578384082466
Gopal1999 <36383789+Gopal1999@users.noreply.github.com>
2022-05-02 17:38:22 +0530

    Update SpeMajorBackendApplication.java

--- src/main/java/com/example/spemajorbackend/SpeMajorBackendApplication.java
+++ src/main/java/com/example/spemajorbackend/SpeMajorBackendApplication.java
@@ -8,10 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @EnableJpaRepositories(basePackageClasses = {StoragePointRepo.class, ReviewRepo.class, VendorRepo.class})
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SpeMajorBackendApplication {
 
     public static void main(String[] args) {
