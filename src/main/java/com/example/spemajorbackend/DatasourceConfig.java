b77bf41aafbc848de63bac5db0e30772ef636514
Gopal1999 <36383789+Gopal1999@users.noreply.github.com>
2022-05-02 17:27:27 +0530

    Update DatasourceConfig.java

--- src/main/java/com/example/spemajorbackend/DatasourceConfig.java
+++ src/main/java/com/example/spemajorbackend/DatasourceConfig.java
@@ -2,28 +2,41 @@ package com.example.spemajorbackend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+// import org.springframework.context.annotation.Primary;
+// import org.springframework.context.annotation.PropertySource;
+// import org.springframework.jdbc.datasource.DriverManagerDataSource;
+// import org.springframework.transaction.annotation.EnableTransactionManagement;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import javax.sql.DataSource;
 
+// @Configuration
+// @EnableTransactionManagement
+// @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+// @PropertySource("classpath:application.properties")
+// public class DatasourceConfig {
+
+//     @Bean
+//     @Primary
+//     public DataSource dataSource() {
+//         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//         dataSource.setUrl(System.getProperty("MYSQL_URL"));
+//         dataSource.setUsername(System.getProperty("MYSQL_USER"));
+//         dataSource.setPassword(System.getProperty("MYSQL_PASSWORD"));
+//         return dataSource;
+//     }
+
+// }
+
 @Configuration
-@EnableTransactionManagement
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-@PropertySource("classpath:application.properties")
 public class DatasourceConfig {
-
     @Bean
-    @Primary
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(System.getProperty("MYSQL_URL"));
-        dataSource.setUsername(System.getProperty("MYSQL_USER"));
-        dataSource.setPassword(System.getProperty("MYSQL_PASSWORD"));
-        return dataSource;
+    public DataSource datasource() {
+        return DataSourceBuilder.create()
+          .driverClassName("com.mysql.cj.jdbc.Driver")
+          .url(System.getProperty("MYSQL_URL"))
+          .username(System.getProperty("MYSQL_USER"))
+          .password(System.getProperty("MYSQL_PASSWORD"))
+          .build();	
     }
-
 }
