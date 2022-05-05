d2fb4d6b6619dbad6b15335423a706cee40d243e
Gopal1999 <goyalgopal359@gmail.com>
2022-05-05 17:37:56 +0530

    testing packages

--- src/main/java/com/example/spemajorbackend/SpeMajorBackendApplication.java
+++ src/main/java/com/example/spemajorbackend/SpeMajorBackendApplication.java
@@ -4,11 +4,16 @@ import com.example.spemajorbackend.repository.ReviewRepo;
 import com.example.spemajorbackend.repository.StoragePointRepo;
 import com.example.spemajorbackend.security.Repository.VendorRepo;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackageClasses = {StoragePointRepo.class, ReviewRepo.class, VendorRepo.class})
+
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = {StoragePointRepo.class, ReviewRepo.class, VendorRepo.class})
 public class SpeMajorBackendApplication {
 
     public static void main(String[] args) {
