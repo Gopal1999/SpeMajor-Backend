fc11f095f4f919f3a932196ad4452394ed5bcbeb
Gopal1999 <goyalgopal359@gmail.com>
2022-05-07 11:10:15 +0530

    .

--- src/main/java/com/example/spemajorbackend/DatasourceConfig.java
+++ src/main/java/com/example/spemajorbackend/DatasourceConfig.java
@@ -1,27 +1,27 @@
-package com.example.spemajorbackend;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-
-@Configuration
-@EnableTransactionManagement
-@PropertySource("classpath:application.properties")
-public class DatasourceConfig {
-
-    @Bean
-    @Primary
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(System.getProperty("MYSQL_URL"));
-        dataSource.setUsername(System.getProperty("MYSQL_USER"));
-        dataSource.setPassword(System.getProperty("MYSQL_PASSWORD"));
-        return dataSource;
-    }
-
-}
+//package com.example.spemajorbackend;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//@EnableTransactionManagement
+//@PropertySource("classpath:application.properties")
+//public class DatasourceConfig {
+//
+//    @Bean
+//    @Primary
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setUrl(System.getProperty("MYSQL_URL"));
+//        dataSource.setUsername(System.getProperty("MYSQL_USER"));
+//        dataSource.setPassword(System.getProperty("MYSQL_PASSWORD"));
+//        return dataSource;
+//    }
+//
+//}
